006fcc90074579b974653846d640fa3e9b434948
Hasinjaka)# <andrianomehasinjaka@gmail.com>
2023-09-17 18:01:41 +0300

    initialize product backend

---
+++ controllers/produits.controller.js
@@ -0,0 +1,29 @@
+const Users = require("../models/User");
+const Roles = require("../models/Role");
+const { Sequelize } = require('sequelize');
+const { sq } = require("../config/db.js");
+
+// create produit
+exports.createProduit = (req, res) => {
+
+} 
+
+// get all produits
+exports.getProduits = (req, res) => {
+
+}
+
+// get one produit
+exports.getProduits = (req, res) => {
+
+}
+
+// delete produit
+exports.deleteProduits = (req, res) => {
+
+}
+
+// update produit
+exports.updateProduits = (req, res) => {
+    
+}
